6c168b805314fe5ee766fca14e51c193581377a9
yashu8685 <yaswanthyendluri557@gmail.com>
2026-06-30 16:40:50 +0530

    third massage

--- FS/settings.py
+++ FS/settings.py
@@ -32,9 +32,16 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
+import os
+
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
+    ".railway.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app",
 ]
 
 
